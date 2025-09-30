f75104b8d5eb6b2f75cf920b365e4c3b70b3184f
ahahau <ahahau1@naver.com>
2025-09-30 18:43:19 +0900

    Fix GridSystem

--- Assets/01.Code/Manager/GridManager.cs
+++ Assets/01.Code/Manager/GridManager.cs
@@ -1,29 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using _01.Code.PathFinder;
-using _01.Code.System.Grid;
+using _01.Code.Players;
+using _01.Code.System;
 using _01.Code.System.Grids;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 namespace _01.Code.Manager
 {
-    public class GridManager : MonoBehaviour
+    public class GridManager : MonoSingleton<GridManager>
     {
-        [field:SerializeField] public int Width { get; private set; }
-        [field:SerializeField] public int Height { get; private set; }
-        [field:SerializeField] public int CellSize { get; private set; }
-        [SerializeField] private List<Obstacle> obstacles;
-        public GridSystem GridSystem{get; private set;}
-        public PathFind PathFinder{get; private set;}
+        [field: SerializeField] public Grid Grid { get; private set; }
+        [field:SerializeField] public Tilemap Tilemap{get; private set;}
+        [SerializeField] private int cellSize = 1;
+        
+        public CommandCenter commandCenter;
+        
+        
+        public Pathfinder PathFinder{get; private set;}
         
         private void Awake()
         {
-            GridSystem = new GridSystem(Width, Height, CellSize);
-            PathFinder = new PathFind(GridSystem);
-            for (int i = 0; i < obstacles.Count ; i++)
-            {
-                obstacles[i].Create();
-            }
+            Grid.cellGap = new Vector3(cellSize,cellSize,0);
+            PathFinder = new Pathfinder(Tilemap);
         }
     }
 }
---
+++ Assets/01.Code/System/MonoSingleton.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace _01.Code.System
+{
+    public class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        public static T Instance { get; private set; }
+        private void Awake()
+        {
+            Instance = FindObjectOfType(typeof(T)) as T;
+        }
+    }
+}
