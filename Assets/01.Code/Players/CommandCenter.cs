acec02b8c4a1dbbe1ebc5d6955f3ac3cedbd570d
ahahau <ahahau1@naver.com>
2025-10-15 23:48:32 +0900

    Fix : CommandCenter

--- Assets/01.Code/Players/CommandCenter.cs
+++ Assets/01.Code/Players/CommandCenter.cs
@@ -1,38 +1,22 @@
 ﻿using System;
 using _01.Code.Entities;
-using _01.Code.System.Grids;
+using _01.Code.PlaceableObjects;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
 namespace _01.Code.Players
 {
-    public class CommandCenter : Entity, IDamageable,IPlaceable
+    public class CommandCenter : PlaceableEntity, IDamageable
     {
-        public Vector3Int Position { get; private set; }
-
-        private void Start()
-        {
-            Initialize(new Vector2Int((int)transform.position.x, (int)transform.position.y));
-        }
-
-        public Tile Tile { get; private set; }
         public void ApplyDamage(int damage, Entity dealer)
         {
-            hp -= damage;
+            maxHp -= damage;
             dealer.OnDeath?.Invoke();
         }
-        public void Initialize(Vector2Int position)
+        public override void Initialize(Vector2Int position = default)
         {
-            Position = new Vector3Int(position.x,position.y, 0);
-            GridManager.Instance.Grid.CellToWorld(Position);
-            Tile = ScriptableObject.CreateInstance<Tile>();
-            SetTile(position);
-        }
-        public void SetTile(Vector2Int tilePos)
-        {
-            GridManager.Instance.Tilemap.SetTile(Position, null);
-            GridManager.Instance.Tilemap.SetTile(new Vector3Int(tilePos.x,tilePos.y,0),Tile);
-            transform.position = GridManager.Instance.Grid.CellToWorld(Position);
+            position = new Vector2Int((int)transform.position.x, (int)transform.position.y);
+            base.Initialize(position);
         }
     }
 }
