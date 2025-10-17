25bf00dca740bca9c85d390b95d53c44a79ecf96
ahahau <ahahau1@naver.com>
2025-10-17 23:22:07 +0900

    Feat : TimeManager

---
+++ Assets/01.Code/Manager/TimeManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace _01.Code.Manager
+{
+    public class TimeManager : MonoBehaviour
+    {
+        public bool IsNightTime { get; private set; }
+        public int CurrentHour { get; private set; } = 0;
+        
+        [field: SerializeField] public int DayTimeHour { get; private set; } = 120;
+        [field:SerializeField] public int NightTimeHour { get; private set; } = 80;
+
+        private bool _runTime = false;
+        public void Initialize()
+        {
+            _runTime = true;
+        }
+
+        private void Update()
+        {
+            
+        }
+    }
+}
